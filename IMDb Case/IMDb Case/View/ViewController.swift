66df15811ca6f8ccd4674ee83eb3447a9b1b5f9e
ahmetfahriyener <extfyener@d-teknoloji.com.tr>
2022-04-09 01:17:49 +0300

    first_commit

---
+++ IMDb Case/IMDb Case/View/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  IMDb Case
+//
+//  Created by ahmetfahriyener on 8.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
